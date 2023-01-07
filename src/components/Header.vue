3c3d8f2d14876ce8aa3d7bcd23f8f7eed1cbd063
Adriana Giraldo <1531nana@gmail.com>
2023-01-06 22:26:05 -0500

    I added the header component with its options, whitout the styles

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,11 @@
 <template>
-	<nav class="navbar navbar-expand-lg navbar-light bg-light">
-		<a class="navbar-brand" href="#">Navbar</a>
+	<nav
+		class="navbar navbar-expand-lg navbar-dark bg-dark"
+		style="background-color: black"
+	>
+		<a class="navbar-brand" href="#"
+			><img src="../assets/Quanspire.png" alt=""
+		/></a>
 		<button
 			class="navbar-toggler"
 			type="button"
@@ -15,20 +20,50 @@
 		<div class="collapse navbar-collapse" id="navbarNav">
 			<ul class="navbar-nav">
 				<li class="nav-item active">
-					<a class="nav-link" href="#"
-						>Home <span class="sr-only">(current)</span></a
-					>
+					<a class="nav-link" href="#">Overview</a>
 				</li>
 				<li class="nav-item">
-					<a class="nav-link" href="#">Features</a>
+					<a class="nav-link active" href="#">Data Model</a>
 				</li>
 				<li class="nav-item">
-					<a class="nav-link" href="#">Pricing</a>
+					<a class="nav-link" href="#">Analytics</a>
 				</li>
 				<li class="nav-item">
-					<a class="nav-link disabled" href="#">Disabled</a>
+					<a class="nav-link" href="#">Markets</a>
 				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#">Optimizer</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#">Hedge</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#">Trader</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#">Supervisor</a>
+				</li>
+				<div>
+					<div class="nav-link">Currencias</div>
+					<div class="nav-link">Commodities</div>
+				</div>
+                <div>
+                    <p class="nav-link">Iván Herrera</p>
+                    <img src="../assets/users.png" alt="" style="height: 10px"/>
+                </div>
+				<div>
+					<div class="nav-link">Light</div>
+					<div class="nav-link">Dark</div>
+				</div>
 			</ul>
 		</div>
+                    <img src="../assets/off.png" alt="" />
 	</nav>
 </template>
+
+
+<style>
+#app .bg-dark{
+    background: black !important;
+}
+</style>
